328bd7c14eafb900411ec09c6bea17b5e74b2263
Sam <sam@boers.family>
2022-12-07 11:15:23 +0100

    made leaderboard look nicer

--- src/components/Leaderboard.svelte
+++ src/components/Leaderboard.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { openSocket } from "../modules/webSockets";
     import { leaderboard } from "../models/leaderboardModel";
+    import { Table } from "sveltestrap";
 
     onMount(() => {
         openSocket();
@@ -9,11 +10,22 @@
 </script>
 
 <h1>Leaderboard</h1>
-<table>
-    {#each $leaderboard as item}
-        <tr>
-            <td>{item.Username}</td>
-            <td>{item.TotalWeight}</td>
-        </tr>
-    {/each}
-</table>
+{#each $leaderboard as item}
+    
+<Table striped>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Username</th>
+        <th>Total weight lifted</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>{item.Username}</td>
+        <td>{item.TotalWeight}</td>
+      </tr>
+    </tbody>
+  </Table>
+{/each}
